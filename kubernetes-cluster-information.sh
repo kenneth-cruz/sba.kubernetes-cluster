f6eb1a1ba64d7a907ac9329dd6252bc1fedf8926
Kenneth Cruz <kenneth.s.cruz@gmail.com>
2021-01-19 08:31:31 -0500

    scripts for deploy, service, node, and pod, ip info, and dump into output file

--- kubernetes-cluster-information.sh
+++ kubernetes-cluster-information.sh
@@ -1,4 +1,8 @@
+ echo '### deployment information ###'
  kubectl describe deployment app-sba-kubernetes-cluster
+ echo '### service information ###'
  kubectl describe service service-sba-kubernetes-cluster
+ echo '### node information ###'
  kubectl get node  
+ echo '### pod information ###'
  kubectl get pods
